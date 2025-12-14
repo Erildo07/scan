5becb38238a501ea23149a9b41dd85ead27f6881
Erildo07 <erildonunes12@gmail.com>
2025-12-14 14:18:29 -0300

    loop criando função

--- loop3.sh
+++ loop3.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-network="192.168.1"
+network="192.168.0"
 
 for i in {1..254}; do
- 
-	ping -c2 $network.$i
+    status=$(ping -c2 $network.$i)
 
-done 
+    resp=$(echo "$status" | grep received | cut -d"," -f2 | cut -d" " -f2)
+
+    if (( resp > 0 )); then
+        echo "O host está ativo:" $network.$i
+    else
+        echo "O host está inativo:" $network.$i
+    fi
+done
